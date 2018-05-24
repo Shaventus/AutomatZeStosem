c1f79356dddc4e1aba6270aea5af6c0dd26cd815
Shaventus <maciej.ciosk@gmail.com>
2018-05-24 22:21:18 +0200

    Operacja Działaj

--- AutomatZeStosem/Automat.cs
+++ AutomatZeStosem/Automat.cs
@@ -8,9 +8,45 @@ namespace AutomatZeStosem
 {
     class Automat
     {
-        private List<String> zbiór_symboli_terminalnych;
-        private List<String> skończony_zbiór_stanów;
-        private Stack<String> stos;
-        private int głowica;
+        private List<char> zst;//zbiór_symboli_terminalnych
+        private List<char> szs;//skończony_zbiór_stanów
+        private Stack<char> stos;
+        private char stan;
+        private bool akceptacja;
+
+        public Automat(List<char> zst, List<char> szs)
+        {
+            this.zst = zst;
+            this.szs = szs;
+            stos = new Stack<char>();
+            akceptacja = true;
+        }
+
+        private void Działaj(char znak)
+        {
+            for(int i = 0; i < zst.Count(); i++)
+            {
+                if(znak == zst[i])
+                {
+                    for (int j = 0; ij < szs.Count(); j++)
+                    {
+                        if (stan == szs[j);
+                        stos.Push(znak);
+                        stan = szs[i];
+                    }
+                }
+            }
+        }
+
+        public bool Operacja(String wyraz)
+        {
+            wyraz = String.Concat(wyraz, "$");
+            stos.Push('#');
+            for (int i = 0;i < wyraz.Length; i++)
+            {
+                Działaj(wyraz[i]);
+            }
+            return akceptacja;
+        }
     }
 }
