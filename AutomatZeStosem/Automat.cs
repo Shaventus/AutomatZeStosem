c2cd7a72e53e57bccfbbd5ba8508ccf978295ce4
Shaventus <maciej.ciosk@gmail.com>
2018-05-24 21:06:44 +0200

    Automat.cs

---
+++ AutomatZeStosem/Automat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatZeStosem
+{
+    class Automat
+    {
+        private List<String> zbiór_symboli_terminalnych;
+        private List<String> skończony_zbiór_stanów;
+        private Stack<String> stos;
+        private int głowica;
+    }
+}
