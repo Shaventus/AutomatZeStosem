561f93acfd41060b26ee047050806b7bf87462d0
Shaventus <maciej.ciosk@gmail.com>
2018-05-26 11:27:48 +0200

    Stos

--- AutomatZeStosem/Automat.cs
+++ AutomatZeStosem/Automat.cs
@@ -10,18 +10,37 @@ namespace AutomatZeStosem
     class Automat
     {
         Tabelka tabelka;
+        Tabelka tabelkaStos;
+        TabelkaStos tstos;
         private Stack<char> stos;
         private int stan;
 
-        public Automat(Tabelka tabelka)
+        public Automat(Tabelka tabelka, Tabelka tabelkaStos, TabelkaStos tstos)
         {
             this.tabelka = tabelka;
+            this.tabelkaStos = tabelkaStos;
+            this.tstos = tstos;
             stos = new Stack<char>();
         }
 
         private void Działaj(char znak)
         {
+            Debug.WriteLine(stos.Peek());
+            stan = tstos.pobierzNastepnyStan(stan, stos.Peek());
             stan = tabelka.pobierzNastepnyStan(stan, znak);
+
+            if(stan != -2 && stan != -1)
+            {
+                if (tabelkaStos.pobierzNastepnyStan(stan, znak) == 1)
+                {
+                    stos.Push(znak);
+                    Debug.WriteLine("Stos push:" + tabelkaStos.pobierzNastepnyStan(stan, znak));
+                }
+                else if (tabelkaStos.pobierzNastepnyStan(stan, znak) == 2)
+                {
+                    Debug.WriteLine("Stos pop:" + stos.Pop());
+                }
+            }
             Debug.WriteLine(stan);
         }
 
@@ -32,7 +51,7 @@ namespace AutomatZeStosem
             stan = 0;
             Debug.WriteLine(stan);
             int i = 0;
-            while (stan != -1 && stan != -2)
+            while (stan != -1 && stan != -2 && stos.Count() != 0)
             {
                 Działaj(wyraz[i]);
                 i++;
--- AutomatZeStosem/Program.cs
+++ AutomatZeStosem/Program.cs
@@ -15,16 +15,46 @@ namespace AutomatZeStosem
         [STAThread]
         static void Main()
         {
+            /*ac
             Tabelka tabelka = new Tabelka(4, 4, new List<Char> { '$', 'a', 'b', 'c' });
             tabelka.wklejStan(new List<int> { -1, 1, 0, 2 }, 0);
             tabelka.wklejStan(new List<int> { -1, -1, -1, 3 }, 1);
             tabelka.wklejStan(new List<int> { -1, 3, -1, -1 }, 2);
             tabelka.wklejStan(new List<int> { -2, 1, 3, 2 }, 3);
 
+            Tabelka tabelkaStos = new Tabelka(4, 4, new List<Char> { '$', 'a', 'b', 'c' });
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 0, 0 }, 0);
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 0, 0 }, 1);
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 0, 0 }, 2);
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 0, 0 }, 3);
+
+            TabelkaStos stos = new TabelkaStos(4, 4, new List<Char> { '$', 'a', 'b', 'c' });
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 0, 0 }, 0);
+            tabelkaStos.wklejStan(new List<int> { 1, 1, 1, 1 }, 1);
+            tabelkaStos.wklejStan(new List<int> { 2, 2, 2, 2 }, 2);
+            tabelkaStos.wklejStan(new List<int> { 3, 3, 3, 3 }, 3);
+            */
+
+            // a^n b^n
+            Tabelka tabelka = new Tabelka(3, 3, new List<Char> { '$', 'a', 'b'});
+            tabelka.wklejStan(new List<int> { -1, 0, 1}, 0);
+            tabelka.wklejStan(new List<int> { -1, -1, 1}, 1);
+            tabelka.wklejStan(new List<int> { -2, -1, -1}, 2);
+
+            Tabelka tabelkaStos = new Tabelka(3, 3, new List<Char> { '$', 'a', 'b' });
+            tabelkaStos.wklejStan(new List<int> { 0, 1, 2 }, 0);
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 2 }, 1);
+            tabelkaStos.wklejStan(new List<int> { 0, 0, 0 }, 2);
+
+            TabelkaStos stos = new TabelkaStos(3, 3, new List<Char> { '#', 'a', 'b'});
+            stos.wklejStan(new List<int> { 0, 0, -1}, 0);
+            stos.wklejStan(new List<int> { 2, 1 , -1}, 1);
+            stos.wklejStan(new List<int> { -2, -1, -1 }, 2);
+
             tabelka.wypisz();
 
-            Automat automat = new Automat(tabelka);
-            String wyraz = "ac";
+            Automat automat = new Automat(tabelka, tabelkaStos, stos);
+            String wyraz = "aaabbb";
             Debug.WriteLine(automat.Operacja(wyraz));
 
             Application.EnableVisualStyles();
